8e1965a4ce99fe2f65526cb7bc7e0c82146b4d11
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2023-09-16 23:25:49 -0300

    test: add test for create a transaction

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,5 @@
-import fastify from 'fastify'
-import { transactionsRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
 import { env } from './env'
-
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
+import { app } from './app'
 
 app
   .listen({
